bce30e455ea981675d464a94f57cf5f50da53786
Purgatorius <Bartlomiej.Bugala@gmail.com>
2017-06-09 12:06:50 +0200

    Create code_LettersCounter.java

---
+++ code_LettersCounter.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Created by B4RT on 2017-06-09.
+ */
+public class Main {
+    public static void main (String[] args)
+    {
+        System.out.println("Hey! Write something and I'll tell you how many letters it has ;)");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        str = str.toLowerCase();
+        int count;
+        for (int i =97; i<123; i++)
+        {
+            count = 0;
+            for (int j = 0; j<str.length(); j++)
+            {
+                if (str.charAt(j)==(char)i)
+                {
+                    count ++;
+                }
+            }
+            if(count>0)
+            {
+                System.out.println("#" + (char)i+ " " + count);
+            }
+        }
+    }
+}
